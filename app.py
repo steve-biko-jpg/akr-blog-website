7758cb330a389029b5ed40162670d51adf86f67f
steve-biko-jpg <sbiko049@gmail.com>
2025-12-01 04:31:46 -0800

    Prepare Flask App for Deployment

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
