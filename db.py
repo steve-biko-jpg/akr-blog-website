2fc228563bf54154f2bd65f097f79768762b21e3
steve-biko-jpg <sbiko049@gmail.com>
2025-12-02 05:08:58 -0800

    Small change on db

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host=os.environ.get("DB_HOST", "localhost"),       # Render host or default localhost
-        user=os.environ.get("DB_USER", "root"),           # Render user or local user
-        password=os.environ.get("DB_PASSWORD", "STeVe!@a#kr5239"),   # Render password or local password
-        database=os.environ.get("DB_NAME", "akrblog"),    # Render database or local db
+        host=os.environ.get("MYSQLHOST", "localhost"),       # Render host or default localhost
+        user=os.environ.get("MYSQLUSER", "root"),           # Render user or local user
+        password=os.environ.get("MYSQLPASSWORD", "STeVe!@a#kr5239"),   # Render password or local password
+        database=os.environ.get("MYSQLPASSWORD", "akrblog"),    # Render database or local db
         cursorclass=pymysql.cursors.DictCursor
     )
